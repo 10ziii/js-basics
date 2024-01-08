75643441d17f0c4fe66fadf7652e32f507034a1b
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-08 16:58:44 +0000

    objects

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,16 +1,32 @@
 // ******************** ARRAY ****************** //
 const array1 = ["toString","toPrecision","toFix","toLocaleString"];
-// console.log(array1);
+console.log(array1);
 // console.log(array1.length);
 // console.log(array1[2]);
 const array2 = new Array(10).fill(-1);
 // console.log(array2);
-// const newArray = array1.join();//converst our array into string
+// const newArray = array1.join();//convert our array into string
 // console.log(newArray);
 // console.log(typeof newArray);
 
 const deleted = array1.splice(1,2,"item1");
 console.log(array1)
 console.log(deleted)
-// array1.slice(1,2)
+console.log(array1.slice(1,2))
+console.log(array1)
 //slice doesnot mutate our original array but splice do
+
+const marvel = ["thor","ironman","hulk"];
+const dc = ["superman","batman","flash"];
+//const superheroes = marvel.concat(dc)
+const superheroes = [...marvel,...dc];
+// console.log(superheroes);
+// console.log({..."abc"})
+
+// const name="lucy";
+// console.log(typeof name)
+// console.log(Array.isArray(name))
+// const name1 = Array.from(name)
+// console.log(typeof name1)
+// console.log(Array.isArray(name1))
+
---
+++ 02_basics/02_object.js
@@ -0,0 +1,9 @@
+// ******************* OBJECTS ********************* //
+const person = {
+    name:"tenziii",
+    age:24,
+    profile:"software developer",
+    location:"california"
+}
+
+
