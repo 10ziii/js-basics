09e9301273fdfae523c77a71fb5e77091f104b9e
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-08 07:13:14 +0000

    operations

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -4,3 +4,8 @@ console.log(typeof(valueinNum));
 console.log(valueinNum);
 /* though js convert our string to a number but we have to thoroughly check
 the value cuz it could be a Nan(not a number) */
+
+// ***************** OPERATIONS ******************* //
+console.log("1" + "2" + 3);// output = 123
+console.log(1 + 2 + "3") // output = 33
+//whatever the first data type, the operation will 
