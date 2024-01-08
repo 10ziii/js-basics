0127d0d9e466014c916938ce733ad12f3b35ee4d
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-08 07:29:30 +0000

    conversionAndOperation

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -9,3 +9,14 @@ the value cuz it could be a Nan(not a number) */
 console.log("1" + "2" + 3);// output = 123
 console.log(1 + 2 + "3") // output = 33
 //whatever the first data type, the operation will 
+//POSTFIX
+ let x = 4;
+ let y = x++;
+ console.log("x is",x);//OUTPUT = 5,incremental is done after evaluation
+ console.log("y is",y);//OUTPUT = 4
+
+//PREFIX
+ let a = 4;
+ let b = ++a;
+ console.log("a is",a);//OUTPUT = 5,INCREMENTAL is done earlier
+ console.log("b is",b);//OUTPUT = 5
