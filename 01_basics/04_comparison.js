9992cd94a3acc0343b4d94e95ce4a758d4214d7c
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-08 08:06:20 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("2" > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log( null >= 0);
+/* when using < > >= <= with null , the null i assigned a value 0 
+"==" only look for values but "===" checks for both datatype and value
+comparison operator < > and equality check == both are different*/
