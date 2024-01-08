e0ea31021bc66101d9fa097ed77545bbba201a7c
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-08 11:43:51 +0000

    datatypes

--- 01_basics/05_dataTypes.js
+++ 01_basics/05_dataTypes.js
@@ -6,4 +6,7 @@ and how to access them
 Primitive data types are stored inside stack while reference are sotred inside heap while
 there reference or address or pointer is stored inside stack
 Javascript is dynamically typed language because the type of variables is determined by the 
-interpreter/compiler at the runtime based on variables value at that time */ 
+interpreter/compiler at the runtime based on variables value at that time 
+change is done on the copy of a value in stack
+change is done on the original value in heap*/ 
+
