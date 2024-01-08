d24753b3abab7860215d75da4cc6937e41c923eb
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-08 08:28:23 +0000

    datatypes

---
+++ 01_basics/05_dataTypes.js
@@ -0,0 +1,9 @@
+/*
+Primitive and non-primitive only two types of datatypes are present in js
+non-primitive is also called reference data type
+in js datatypes are categorised into two based on how they are stored inside memory
+and how to access them
+Primitive data types are stored inside stack while reference are sotred inside heap while
+there reference or address or pointer is stored inside stack
+Javascript is dynamically typed language because the type of variables is determined by the 
+interpreter/compiler at the runtime based on variables value at that time */ 
