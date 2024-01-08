9bb0c5854bb49cf74a74d5d3f80cb8fa244fe06b
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-08 17:39:39 +0000

    object

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -1,9 +1,20 @@
 // ******************* OBJECTS ********************* //
+
+const sym = Symbol("key1")
+const key1 = "key11"
 const person = {
+    [sym]:"key111",
     name:"tenziii",
     age:24,
     profile:"software developer",
-    location:"california"
+    location:"california",
+    key1:"value1"
 }
 
+console.log(person['name']);
+console.log(person[sym])
+console.log(typeof person[sym])
+console.log([key1],person['key1'])
 
+//to avoid making change to objects 
+Object.freeze(person);
