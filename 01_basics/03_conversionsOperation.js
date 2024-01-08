eec067af1f171463939581ff8d492a002cef707f
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-08 06:49:21 +0000

    conversion

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,6 @@
+let score = "33abc" ;
+let valueinNum = Number(score)
+console.log(typeof(valueinNum));
+console.log(valueinNum);
+/* though js convert our string to a number but we have to thoroughly check
+the value cuz it could be a Nan(not a number) */
