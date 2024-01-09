f3e761f604161862869b52b7b2ad44fd20fcbd4e
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-09 16:56:14 +0000

    dom

---
+++ 05_dom/01_dom.js
@@ -0,0 +1,6 @@
+/* DOM stands for document object model, based on observations by the browser to
+out html webpage it creates element of our html page into objects also known as documents 
+which get stored inside window object. we can manipulate alter the contents of
+our webpage using DOM manipulation */
+
+//ALL DOM SELECTOR
