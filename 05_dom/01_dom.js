dc273f26db374c26a72c15e6a8a32efa2624c6e9
10ziii <104753663+10ziii@users.noreply.github.com>
2024-01-10 05:40:26 +0000

    dom

--- 05_dom/01_dom.js
+++ 05_dom/01_dom.js
@@ -4,3 +4,10 @@ which get stored inside window object. we can manipulate alter the contents of
 our webpage using DOM manipulation */
 
 //ALL DOM SELECTOR
+/*
+getElementByClassName : selects element with class name gives us a html collection which is live list no for each
+getElementById : select elements with id
+querySelector : can select anything with this
+querySelectorAll : gives node list static
+ */
+
